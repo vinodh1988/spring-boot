f04788cf5b19d73bd023544941c364655b539376
vinodh1988 <k.c.vinodh@hotmail.com>
2024-08-06 17:34:53 +0530

    jpa

---
+++ src/main/java/com/firstspring/repositories/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.firstspring.repositories;
+
+import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.firstspring.entities.Project;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+}
