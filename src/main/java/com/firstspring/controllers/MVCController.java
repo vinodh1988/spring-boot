7495d892ba337418d5c82c687e9d182d9b92513b
vinodh1988 <k.c.vinodh@hotmail.com>
2024-08-06 15:22:12 +0530

    service-injection

--- src/main/java/com/firstspring/controllers/MVCController.java
+++ src/main/java/com/firstspring/controllers/MVCController.java
@@ -1,15 +1,26 @@
 package com.firstspring.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.firstspring.service.DataService;
 
 @Controller
 @RequestMapping("/app24")
 public class MVCController {
 
+	@Autowired
+	@Qualifier("dservice")
+	private DataService service;
+	
 	 @RequestMapping("/hello")
-	 public String getHome() {
+	 public String getHome(ModelMap m) {
+		 
 		 //business logic
+		 m.addAttribute("team", service.getDevelopers());
 		 return "home";
 	 }
 }
