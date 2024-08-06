5e26e7d88e087c1a6643fbdd4d6d24ad8ea97414
vinodh1988 <k.c.vinodh@hotmail.com>
2024-08-06 16:09:04 +0530

    scopes

--- src/main/java/com/firstspring/controllers/MVCController.java
+++ src/main/java/com/firstspring/controllers/MVCController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.firstspring.service.DataService;
+import com.firstspring.utilities.ApplicationRequestCounter;
+import com.firstspring.utilities.RequestCounter;
 
 @Controller
 @RequestMapping("/app24")
@@ -14,13 +16,25 @@ public class MVCController {
 
 	@Autowired
 	@Qualifier("dservice")
+	
+	
 	private DataService service;
+	
+	@Autowired
+	private RequestCounter counter;
+	
+	@Autowired
+    private ApplicationRequestCounter acounter;
 	
 	 @RequestMapping("/hello")
 	 public String getHome(ModelMap m) {
 		 
 		 //business logic
+		 counter.setCounter();
+		 acounter.setCounter();
 		 m.addAttribute("team", service.getDevelopers());
+		 m.addAttribute("scount",counter.getCounter());
+		 m.addAttribute("acount",acounter.getCounter());
 		 return "home";
 	 }
 }
