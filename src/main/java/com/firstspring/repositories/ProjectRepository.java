d2e02882ed5cd2bd1898b4f53dc195290ecbf883
vinodh1988 <k.c.vinodh@hotmail.com>
2024-08-07 15:58:32 +0530

    jpql

--- src/main/java/com/firstspring/repositories/ProjectRepository.java
+++ src/main/java/com/firstspring/repositories/ProjectRepository.java
@@ -1,7 +1,11 @@
 package com.firstspring.repositories;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.firstspring.entities.Project;
 
@@ -9,5 +13,8 @@ public interface ProjectRepository extends JpaRepository<Project, Integer> {
     public Project findByProjectno(Integer projectno);
     
     public Project findByName(String name);
+    
+    @Query("SELECT p FROM Project p where p.teamsize between :minsize and :maxsize")
+    public List<Project> findByTeamsize(@Param("minsize")int min, @Param("maxsize")int max);
 }
 
