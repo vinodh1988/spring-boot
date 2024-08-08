1c7a345b86eb207430131ed8f1682a3c8bf93981
vinodh1988 <k.c.vinodh@hotmail.com>
2024-08-08 16:00:04 +0530

    repo-dept

---
+++ src/main/java/com/firstspring/controllers/DepartmentController.java
@@ -0,0 +1,5 @@
+package com.firstspring.controllers;
+
+public class DepartmentController {
+
+}
---
+++ src/main/java/com/firstspring/repositories/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.firstspring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.firstspring.entities.Department;
+
+public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+
+}
