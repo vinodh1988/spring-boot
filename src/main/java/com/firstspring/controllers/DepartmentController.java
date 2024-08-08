c52980f5ca23cfc27e859702184af8d8e57e0509
vinodh1988 <k.c.vinodh@hotmail.com>
2024-08-08 16:03:52 +0530

    department-controller

--- src/main/java/com/firstspring/controllers/DepartmentController.java
+++ src/main/java/com/firstspring/controllers/DepartmentController.java
@@ -1,5 +1,23 @@
 package com.firstspring.controllers;
 
-public class DepartmentController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.firstspring.entities.Department;
+import com.firstspring.service.DepartmentService;
+
+@RestController
+@RequestMapping("/api/departments")
+public class DepartmentController {
+	@Autowired
+	private DepartmentService dservice;
+
+	 @GetMapping("")
+	 public List<Department> getDepartments() {
+		 return dservice.getDepartments();
+	 }
 }
